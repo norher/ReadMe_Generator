dc83f4b59a1a6dca69652fa80d773c20f775dde1
Norman Herrera <norman.herrera@ymail.com>
2022-04-26 23:21:13 -0700

    readme template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,8 +12,41 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  #**${data.title}**
 
+  ## Table of contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contribution Guidelines](#contribution-guidelines)
+  - [Test](#test)
+  - [Questions](#questions)
+    - [GitHub](#github)
+    - [email](#email)
+
+  ## Description 
+  ${data.description}
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution Guidelines
+  ${data.contribution}
+
+  ## Test
+  ${data.tests}
+
+  ## License
+  ${data.licenseType}
+
+  ## Questions
+  ### @${data.github}
+  ### ${data.email}
 `;
 }
 
